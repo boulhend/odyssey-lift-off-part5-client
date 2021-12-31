dace6127c6b57124504c6407454381d1ee3d00a4
brahim <boulhend.brahim@gmail.com>
2021-12-31 15:01:14 +0100

    good

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://apollo-course.herokuapp.com",
+  uri: "https://apollo-course.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
